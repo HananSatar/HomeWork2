9679c15f76ed29dccd57e959522fa3faa0793402
HananSatar <41490988+HananSatar@users.noreply.github.com>
2018-11-16 11:05:40 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //count row of file
 var fs=require('fs');
-// filename=process.argv[2];
-
+let _=require('underscore');
  var data=fs.readFileSync('flower.txt');
  var res=data.toString().split('\n').length;
  console.log("count num of row=",res);
@@ -21,25 +20,23 @@ var fs=require('fs');
   return total;
 }
 console.log("count NUM of flower not start with 's'",totalWordCount());
-///4flower that begin with my name
-function fname(){
-  var str=data.toString().split('/n');
-  let sname=[];
-  for(var i=0;i<str.length-1;i++){
-  //sname[i]=str[i];
-  if(sname=(str[i].charAt(0).split('/n')=="H"))
-  { 
-    str[i]= sname[i];
-  }
-  sname[i]=sname[i]+1;
-  }
-  console.log(sname[i])
-  //sname[i]=sname[i]+1;
-   // return console.log(sname);
-  return sname[i];
-  }
-console.log(fname());
 
+// flower name that start with letter 'S' 
+ res=data.toString().split('\n');
+let Start_with_S = _.filter(res, (element) => {
+  return element.startsWith("S");
+});
+console.log("flower name that start with letter 'S' : " + Start_with_S);
+
+// flower name that start with letter 'H' 
+res=data.toString().split('\n');
+let Start_H = _.filter(res, (element) => {
+  return element.startsWith("H");
+});
+console.log("flower name that start with letter 'H' : " + Start_H);
+//list flower that length =5
+var l = _.filter(res, function(flower){ return flower.length==5; });
+console.log("5-List all the flower the name length is 5 \n" + l)
 /////**5 count flower name length 5**/
 function totallenth() {
   //var l1=[str];
@@ -56,16 +53,23 @@ function totallenth() {
  return totalSoFar;
 }
 console.log("count flower name length 5=",totallenth())
-/*var fs=require('fs');
-var data=fs.readFileSync('flower.txt');
-var c=data.toString().split('/n');
-function WordCount(str) { 
-    if(str.charAt(0)=="G")
-    var data=fs.readFileSync('flower.txt');
-    var c=data.toString().split('/n');
-    return list=[str];
-   // return c.split('/n').length.charAt(0)="S";
-  }
   
-  console.log(WordCount(c));
-*/
+/* function fname(){
+  var str=data.toString().split('/n');
+  let sname=[];
+  for(var i=0;i<str.length-1;i++){
+  //sname[i]=str[i];
+  if(sname=(str[i].charAt(0).split('/n')=="H"))
+  { 
+    str[i]= sname[i];
+  }
+  sname[i]=sname[i]+1;
+  }
+  console.log(sname[i])
+  //sname[i]=sname[i]+1;
+   // return console.log(sname);
+  return sname[i];
+  }
+console.log(fname());*/
+
+
